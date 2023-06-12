53186b932686b71f7df7723a48a2585cca83f5b3
JigneshNakum16 <jigneshnakum16@gmail.com>
2023-06-12 18:11:35 +0530

    fix:add store

--- pages/api/createCoffeeStore.js
+++ pages/api/createCoffeeStore.js
@@ -1,32 +1,59 @@
-const Airtable = require('airtable');
-const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(process.env.AIRTABLE_BASE_ID);
+const Airtable = require("airtable");
+const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(
+  process.env.AIRTABLE_BASE_ID
+);
 
-const table = base('coffee-stores')
-
+const table = base("coffee-stores");
 
 const createCoffeeStore = async (req, res) => {
-    if (req.method === "POST") {
-        //find record
-        const findCoffeeStoreRecoreds = await table.select({
-            filterByFormula: `id="0"`
-        }).firstPage()
-        
-        // const { id, name, neighbourhood, address, imgUrl, voting } = req.body;
-        // console.log("@@@@@@",id)
-        console.log("findCoffeeStoreRecoreds",
-            findCoffeeStoreRecoreds
-        )
+  console.log("req", req);
+  console.log("req.body", req.body);
+  const { id, name, neighbourhood, address, imgUrl, voting } = req.body;
+  if (req.method === "POST") {
+    //find record
+    try {
+      const findCoffeeStoreRecords = await table
+        .select({
+          filterByFormula: `id=${id}`,
+        })
+        .firstPage();
 
-        if (findCoffeeStoreRecoreds.length !== 0) {
-            
-            res.json(findCoffeeStoreRecoreds)
-        } else {
-            //create record
-            res.json({ message: "create a record" })
-        }
+      if (findCoffeeStoreRecords.length !== 0) {
+        const records = findCoffeeStoreRecords.map((record) => {
+          return {
+            ...record.fields,
+          };
+        });
 
+        res.json(records);
+      } else {
+        //create record
+        const createRecord = await table.create([
+          {
+            fields: {
+              id,
+              name,
+              neighbourhood,
+              address,
+              imgUrl,
+              voting,
+            },
+          },
+        ]);
 
+        const records = createRecord.map((record) => {
+          return {
+            ...record.fields,
+          };
+        });
+        res.json({ records });
+      }
+    } catch (error) {
+      console.log("Error Finding Store", error);
+      res.status(500);
+      res.json({ message: "Something went wrong", error });
     }
-}
+  }
+};
 
-export default createCoffeeStore
+export default createCoffeeStore;
